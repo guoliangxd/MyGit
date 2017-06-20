ed36fbf5070fc34d77815278aa44de3e69f99cb6
guoliang <995445680@qq.com>
2017-06-20 20:10:25 +0800

    changeslogs

---
+++ app/src/main/java/com/guoliang/dnote/app/App.java
@@ -0,0 +1,18 @@
+package com.guoliang.dnote.app;
+
+import android.app.Application;
+
+import com.avos.avoscloud.AVOSCloud;
+
+/**
+ * Created by XhinLiang on 2017/5/13.
+ * xhinliang@gmail.com
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AVOSCloud.initialize(this, "4tr8DCDJVfHMq4fzWlf4Ey5D-gzGzoHsz", "b0bK6SrjcTURP1gLHtHL4O5t");
+    }
+}
